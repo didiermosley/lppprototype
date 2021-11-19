83e9b2554f504084d695961564d17a19b26a5bb4
Didier <blessingmosley@hotmail.com>
2021-11-18 20:56:51 -0500

    Final version of the prototype so far.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const closeBtn = document.getElementById('close-btn');
 const nav = document.getElementById('nav');
 const transparent = document.querySelector(".glass-bg");
 
-
 toggle.addEventListener("click",()=>{
     nav.classList.toggle("open");
     transparent.classList.toggle('appear');
@@ -18,3 +17,25 @@ function close(){
 closeBtn.addEventListener("click",close);
 transparent.addEventListener("click",close);
 
+
+// Dark-light darkMode.
+const darkMode = document.getElementById('mode');
+const section = document.querySelector(".section");
+const zero=document.getElementById('0');
+const body = document.querySelector(".body");
+const ul=document.querySelector(".ul-list");
+const circle=document.querySelector(".info-ico");
+const logos= document.querySelector(".svg");
+const cards = document.querySelector(".cards");
+
+let bg = '#3A3B3C';
+let colorD1='#e4e6eb';
+
+function change(){
+    body.classList.toggle("light-bg");
+    section.classList.toggle("change-color");
+    cards.classList.toggle("light-bg");
+    logos.classList.toggle("light");
+}
+
+darkMode.addEventListener("click",change);
